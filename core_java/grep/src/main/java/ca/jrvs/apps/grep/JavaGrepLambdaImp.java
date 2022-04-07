7b44ef87f3141c75ba0237ef48ce42df69940879
Binsar Hutapea <binsar.pragiwaksana@gmail.com>
2022-04-07 07:43:54 +0000

    Implemented Lambda and Stream API for grep

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepLambdaImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepLambdaImp.java
@@ -1,23 +1,53 @@
 package ca.jrvs.apps.grep;
 
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class JavaGrepLambdaImp extends JavaGrepImp{
 
     @Override
-    public List<File> listFiles(String rootDir) {
-        return listFiles().stream()
+    public List<File> listFiles(String rootDir){
+
+        List<File> filesList = new ArrayList<>();
+
+        try (Stream<Path> pathStream = Files.walk(Paths.get(rootDir), Integer.MAX_VALUE)){
+            pathStream.forEach(path -> {
+                if (path.toFile().isFile()){
+                    filesList.add(path.toFile());
+                }
+            });
+        } catch (IOException e) {
+            this.logger.error("IO Exception", e);
+        }
+
+        return filesList;
     }
 
     @Override
-    public List<String> readLines(File inputFile) {
-        return super.readLines(inputFile);
+    public List<String> readLines(File inputFile){
+        List<String> linesList = new ArrayList<>();
+
+        try (Stream<String> lineStream = Files.lines(inputFile.toPath())){
+            lineStream.forEach(line -> linesList.add(line));
+        } catch (IOException e) {
+            this.logger.error("IO Exception", e);
+        }
+
+        return linesList;
     }
 
     public static void main(String[] args){
 
-        if (args.length != 3) {
+        if (args.length == 3) {
 
             JavaGrepLambdaImp javaGrepLambdaImp = new JavaGrepLambdaImp();
             javaGrepLambdaImp.setRegex(args[0]);
@@ -27,9 +57,8 @@ public class JavaGrepLambdaImp extends JavaGrepImp{
             try {
                 javaGrepLambdaImp.process();
             } catch (Exception ex){
-                ex.printStackTrace();
+                javaGrepLambdaImp.logger.error("Exception", ex);
             }
-
 
         }
 
