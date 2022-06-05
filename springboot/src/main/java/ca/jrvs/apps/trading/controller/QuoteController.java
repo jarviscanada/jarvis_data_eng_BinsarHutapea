52bfd96117e51d0e8a8848eaff0da38e75ab603c
Binsar Hutapea <binsar.pragiwaksana@gmail.com>
2022-06-05 19:40:36 +0000

    Swagger setup

--- springboot/src/main/java/ca/jrvs/apps/trading/controller/QuoteController.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/controller/QuoteController.java
@@ -2,12 +2,18 @@ package ca.jrvs.apps.trading.controller;
 
 import ca.jrvs.apps.trading.model.domain.IexQuote;
 import ca.jrvs.apps.trading.service.QuoteService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+//@Api(value = "quote", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 @Controller
 @RequestMapping("/quote")
 public class QuoteController {
@@ -19,6 +25,8 @@ public class QuoteController {
         this.quoteService = quoteService;
     }
 
+    //@ApiOperation(value = "Show iexQuote", notes = "Show iexQuote for a given ticker/symbol")
+    //@ApiResponses(value = {@ApiResponse(code = 404, message ="ticker is not found")})
     @GetMapping(path = "/iex/ticker/{ticker}")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
